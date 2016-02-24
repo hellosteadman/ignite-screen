5c55cd09bb76b5be909bffe8b1c8584baaf79ad5
Mark <mark@steadman.io>
2016-02-24 11:50:30 +0000

    Remove stray context processor

--- screen/context_processors.py
+++
@@ -1,8 +0,0 @@
-from bambu_pages.models import Page
-from django.conf import settings
-
-def pages(request):
-    return {
-        'root_pages': Page.objects.filter(parent__isnull = True),
-        'GOOGLE_ANALYTICS_ID': settings.GOOGLE_ANALYTICS_ID
-    }
